c87dd33fbcadc4334ef2fff695e120e11dcdbb70
Ben Arno <bdarno92@gmail.com>
2023-01-17 13:50:47 -0800

    401-2 complete, files reorganized

---
+++ Ops401/Challenge2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import subprocess
+import time
+from datetime import datetime
+
+
+print("Please enter an IP to ping.")
+adrs = input()
+
+while True:
+  logs = open('logs.txt', 'a+')
+  lstPng = subprocess.run(['ping', '-c', '1', adrs], capture_output=True).returncode
+  stamp = datetime.now()
+  if lstPng == 0:
+    result = f'{stamp}, {adrs}, Network Active'
+  else:
+    result = f'{stamp}, {adrs}, Oh no.'
+  print(result)
+  logs.writelines(f'{result}\n')
+  time.sleep(2)
