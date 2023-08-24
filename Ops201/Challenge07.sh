3ad17da43c3db970ef5b783ef114cd0db08d6d5a
Ben Arno <bdarno92@gmail.com>
2023-08-24 21:48:55 +0000

    I am done with this challenge.

--- Ops201/Challenge07.sh
+++ Ops201/Challenge07.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+echo "System name:"
+hostname
+
+stats=`lshw`
+
+echo "CPU:"
+echo $stats | grep "*-cpu" -A 5
+
+echo "RAM"
+echo $stats | grep "*-memory" -A 3
+
+echo "Display Adapter"
+echo $stats | grep "*-display" -A 10
+
+echo "Network Adapter"
+echo $stats | grep "*-network" -A 15
+
+# Inquiring minds knowledgable in bash may look at this and say hey, that's going to work poorly. This is true, but I don't want to run lshw five times and I'm tired of messing with this.
